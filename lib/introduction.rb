bf55283a367f4dc38dea818a2af803700ed46101
rudi hinds <rudihinds@gmail.com>
2019-04-28 00:12:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
-def introduction(name)
-  puts "Hi, my name is #{name} and I am learning to program in #{}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction("Dan", "Ember.js")
